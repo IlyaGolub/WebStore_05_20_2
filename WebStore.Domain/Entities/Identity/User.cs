e5797d684076ab7a77d96e5d3b4f2ca3269b42df
IlyaGolub <47134681+IlyaGolub@users.noreply.github.com>
2020-06-16 19:01:30 +1000

    Update User.cs

--- WebStore.Domain/Entities/Identity/User.cs
+++ WebStore.Domain/Entities/Identity/User.cs
@@ -5,6 +5,8 @@ namespace WebStore.Domain.Entities.Identity
     public class User : IdentityUser
     {
         public const string Administrator = "Admin";
-        public const string DefaultAdminPassword = "AdminPAssword";
+        public const string DefaultAdminPassword = "AdminPAssword"; 
+        public const string Moderator = "Moder";
+        public const string DefaultModerPassword = "ModerPAssword";
     }
 }
